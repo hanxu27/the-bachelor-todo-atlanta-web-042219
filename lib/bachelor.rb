a4829f620c84ee50a926dd9dc1cdd2f04a575f30
han xu <hanxu27@gmail.com>
2019-04-15 19:32:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,7 +19,6 @@ end
 def get_contestant_name(data, occupation)
   # code here
   data.each do |key, players|
-    binding.pry
     players.each do |player_data|
       if occupation == player_data["occupation"]
         return player_data["name"]
